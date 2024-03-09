75d5835e53b15818f8fbbf37863a3c7ff3db44d5
barabane <barabayne@gmail.com>
2024-03-09 11:59:10 +0500

    Index page
    Add index.html
    Add index.css
    Add favicon icon
    Connect PicoCSS to the html template

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='static')
+
+
+@app.route('/', methods=["GET"])
+def index_handler():
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run()

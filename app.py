22c8c8004c4a228b061b2f46a61a354380e1efc8
barabane <barabayne@gmail.com>
2024-03-11 21:56:07 +0500

    Connect login manager

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
+import os
+
 from flask import Flask, render_template
+from flask_login import LoginManager
+from db import db
 
 from blueprints.url_blueprint import url as url_blueprint
 
 app = Flask(__name__, template_folder='static')
+app.secret_key = os.getenv('SECRET_KEY')
+
+login_manager = LoginManager()
 
 
 @app.route('/', methods=["GET"])
@@ -10,7 +17,13 @@ def index_handler():
     return render_template("index.html")
 
 
+@login_manager.user_loader
+def user_loader_handler(user_id):
+    return db.get_user_by_id(user_id=user_id)
+
+
 if __name__ == "__main__":
     app.register_blueprint(url_blueprint)
+    login_manager.init_app(app=app)
 
     app.run()

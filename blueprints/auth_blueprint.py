37d19b8883c4a1c16090062db6cd59680ed58054
barabane <barabayne@gmail.com>
2024-03-13 12:49:18 +0500

    Auth block
    Fix signin/signup handlers
    Create auth.html page instead
    of modal window
    Delete modal window

--- blueprints/auth_blueprint.py
+++ blueprints/auth_blueprint.py
@@ -7,10 +7,10 @@ from forms.AuthForm import AuthForm
 auth = Blueprint(name="auth", url_prefix="/auth/", import_name="auth")
 
 
+@auth.route('/')
 @auth.route('/signin', methods=["GET", "POST"])
 def signin_handler():
     form = AuthForm()
-
     if form.validate_on_submit():
         email = form.email.data
         password = form.password.data
@@ -18,19 +18,19 @@ def signin_handler():
         user = db.get_user_by_email(email=email)
         if not user:
             flash(message='Такого пользователя не существует', category='error')
-            return redirect(url_for('index_handler'))
+            return render_template('auth.html', form=form)
 
         if not check_password_hash(pwhash=user.password, password=password):
-            flash(message='Неправильный пароль', category='error')
-            return redirect(url_for('index_handler'))
+            flash(message='Неправильный логин/пароль', category='error')
+            return render_template('auth.html', form=form)
         return redirect(url_for('index_handler'))
-    return render_template('index.html', form=form)
+    return render_template('auth.html', form=form)
 
 
+@auth.route('/')
 @auth.route('/signup', methods=["GET", "POST"])
 def signup_handler():
     form = AuthForm()
-
     if form.validate_on_submit():
         email = form.email.data
         password = form.password.data
@@ -38,8 +38,8 @@ def signup_handler():
         user_exists = db.get_user_by_email(email=email)
         if user_exists:
             flash(message='Такой пользователь уже существует', category='error')
-            return redirect(url_for('index_handler'))
+            return render_template('auth.html', form=form)
 
         db.register_user(email=email, password=password)
         return redirect(url_for('index_handler'))
-    return render_template('index.html', form=form)
+    return render_template('auth.html', form=form)
--- forms/AuthForm.py
+++ forms/AuthForm.py
@@ -4,11 +4,11 @@ from wtforms.validators import Email, DataRequired, Length
 
 
 class AuthForm(FlaskForm):
-    email = EmailField('email', validators=[
+    email = EmailField('Email', validators=[
         DataRequired(message='Введите Email'),
         Email(message='Введите валидный Email')
     ])
-    password = PasswordField('password', validators=[
+    password = PasswordField('Пароль', validators=[
         DataRequired(message='Введите пароль'),
         Length(min=8, message='Минимальная длинна пароля 8 символов'),
         Length(max=20, message='Максимальная длинна пароля 20 символов')
